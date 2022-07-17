66c1a2c118b711e11d17122da0f204d3701b6e47
farhad <rb0214192@gmail.com>
2022-07-17 17:20:22 +0430

    products and dummydata are added

---
+++ src/data/dummyData.js
@@ -0,0 +1,15 @@
+export const rateData = [
+  { string: "1", number: 1 },
+  { string: "2", number: 2 },
+  { string: "3", number: 3 },
+  { string: "4", number: 4 },
+  { string: "5", number: 5 },
+];
+
+export const categoryData = [
+  { value: "", name: "All", id:"1" },
+  { value: "men's clothing", name: "Men's Clothing", id:"2" },
+  { value: "jewelery", name: "Jewelery" , id:"3"},
+  { value: "electronics", name: "Electronics", id:"4" },
+  { value: "women's clothing", name: "Women's clothing", id:"5" },
+];
