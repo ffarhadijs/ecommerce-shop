304a7a58e7a16bc5f5295f6697b6f8cc0b7b8371
farhad <rb0214192@gmail.com>
2022-07-19 08:57:39 +0430

    app file is added with pages link

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import { Provider } from "react-redux/es/exports";
+import { store } from "./app/store";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
-    <div className="app">
+    <Provider store={store}>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/shop" element={<Shop />} />
         <Route exact path="/shop/:id" element={<DetailPage />} />
+        <Route exact path="/cart" element={<Cart />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+    </Provider>
   );
 };
 
