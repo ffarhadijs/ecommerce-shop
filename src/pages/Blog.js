0be5941dd87f7040f3d63976bff8ce8bbd7d38dd
farhad <rb0214192@gmail.com>
2022-07-24 10:45:43 +0430

    blog page created

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import BlogList from '../component/blog/blogList/BlogList'
-import LayoutWrapper from '../component/HOC/LayoutWrapper'
-
+import React from "react";
+import BlogList from "../component/blog/blogList/BlogList";
+import LayoutWrapper from "../component/HOC/LayoutWrapper";
+import {Link} from "react-router-dom"
 const Blog = () => {
   return (
     <div>
-      <BlogList/>
-      
+      <div className=" w-full h-32 bg-gradient-to-r text-xl from-gray-50 via-gray-200 to-gray-50 flex flex-row justify-center items-center">
+        <Link to="/" className=" text-gray-600 ">
+          BoShop
+        </Link>
+        <span className="px-2">/</span>
+        <span className=" text-black "> Blog List</span>
+      </div>
+      <BlogList />
     </div>
-  )
-}
+  );
+};
 
-export default LayoutWrapper(Blog)
+export default LayoutWrapper(Blog);
