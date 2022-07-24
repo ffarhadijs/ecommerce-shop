a635f9b0fac604da80b4dc2ff48bf4d826aca0bf
farhad <rb0214192@gmail.com>
2022-07-24 10:37:49 +0430

    fixed some tailwind classe

--- src/component/blog/blogList/BlogList.js
+++ src/component/blog/blogList/BlogList.js
@@ -55,13 +55,7 @@ const BlogList = () => {
   };
   return (
     <div className="bg-gray-50 px-8">
-      <div className=" w-full h-32 bg-gradient-to-r text-xl from-gray-50 via-gray-200 to-gray-50 flex flex-row justify-center items-center">
-        <Link to="/" className=" text-gray-600 ">
-          BoShop
-        </Link>
-        <span className="px-2">/</span>
-        <span className=" text-black "> Blog</span>
-      </div>
+
 
       <div className="flex flex-row items-start justify-center">
         <div className="w-1/5 flex flex-col justify-start items-center py-10">
