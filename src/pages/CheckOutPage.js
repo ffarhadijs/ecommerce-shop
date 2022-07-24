37e990ff3a03161a1fba760e0390333bc909ce4c
farhad <rb0214192@gmail.com>
2022-07-24 10:43:50 +0430

    checkout page added

---
+++ src/pages/CheckOutPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Billing from "../component/checkOut/Billing";
+import Order from "../component/checkOut/Order";
+import Shipping from "../component/checkOut/Shipping";
+import LayoutWrapper from "../component/HOC/LayoutWrapper";
+import { Link } from "react-router-dom";
+const CheckOutPage = () => {
+  return (
+    <div>
+      <div className=" w-full h-32 bg-gradient-to-r text-xl from-gray-50 via-gray-200 to-gray-50 flex flex-row justify-center items-center">
+        <Link to="/" className=" text-gray-600 ">
+          BoShop
+        </Link>
+        <span className="px-2">/</span>
+        <Link to="/cart" className=" text-gray-600">
+          cart
+        </Link>
+        <span className="px-2">/</span>
+        <span className=" text-gray-600"> Check Out</span>
+      </div>
+      <div className="w-5/6 mx-auto flex flex-row justify-center items-start">
+        <div className="w-2/3">
+          <Billing />
+          <Shipping />
+        </div>
+        <div className="w-1/3">
+          <Order />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LayoutWrapper(CheckOutPage);
