1cbcb80879b9d8ad53ba3efda54f24a21f28ff4b
farhad <rb0214192@gmail.com>
2022-07-24 10:45:03 +0430

    breadCrumbs added

--- src/pages/BlogDetailPage.js
+++ src/pages/BlogDetailPage.js
@@ -2,9 +2,21 @@ import React from 'react'
 import BlogDetails from '../component/blogDetails/BlogDetails'
 import Comments from '../component/comments/Comments'
 import LayoutWrapper from '../component/HOC/LayoutWrapper'
+import {Link} from "react-router-dom"
 const BlogDetailPage = () => {
   return (
     <div>
+      <div className=" w-full h-32 bg-gradient-to-r text-xl from-gray-50 via-gray-200 to-gray-50 flex flex-row justify-center items-center">
+        <Link to="/" className=" text-gray-600 ">
+          BoShop
+        </Link>
+        <span className="px-2">/</span>
+        <Link to="/blog" className=" text-gray-600 ">
+          Blog List
+        </Link>
+        <span className="px-2">/</span>
+        <span className=" text-black "> Blog</span>
+      </div>
       <BlogDetails/>
       <Comments/>
     </div>
