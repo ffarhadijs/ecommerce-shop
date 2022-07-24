63f9ee491c3273ce1b8528d0c6920267ef5e6870
farhad <rb0214192@gmail.com>
2022-07-24 10:42:29 +0430

    wishSlice completed

---
+++ src/features/wishList/wishSlice.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  wishListItems: [],
+};
+
+export const wishSlice = createSlice({
+  name: "wish",
+  initialState,
+  reducers: {
+    AddToList: (state, action) => {
+      const index = state.wishListItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (index < 0) {
+        state.wishListItems.push({
+          ...action.payload,
+        });
+      } else {
+        state.wishListItems.pop({
+          ...action.payload,
+        });
+      }
+    },
+    Remove:(state,action)=>{
+        state.wishListItems=state.wishListItems.filter(item=>item.id!==action.payload.id)
+    }
+  },
+});
+
+export const { AddToList, Remove } = wishSlice.actions;
+
+export default wishSlice.reducer;
