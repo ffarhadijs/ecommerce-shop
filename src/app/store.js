b9b6ab0d6790d18f0ca0a866ccd20f0221ff4f79
farhad <rb0214192@gmail.com>
2022-07-24 10:35:12 +0430

    wish store added

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import wishReducer from "../features/wishList/wishSlice"
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    wish: wishReducer
   },
 });
