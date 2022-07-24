b4c602b32308ccb20eaa5a4e09b195c3947701b2
farhad <rb0214192@gmail.com>
2022-07-24 10:39:47 +0430

    checkout sections completed

---
+++ src/component/checkOut/Order.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { shorten } from "../../helpers/shorten";
+import { useFormik } from "formik";
+
+const Order = () => {
+  const items = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+  const formik = useFormik({
+    initialValues: {
+      paymentMethod: "",
+      accept: false,
+    },
+  });
+
+  return (
+    <div className="p-4 w-full">
+      <h1 className="text-slate-800 text-2xl font-bold my-6">Your Order</h1>
+      <div className="border border-slate-800 py-6 px-4 flex flex-col justify-center items-start w-full">
+        {items.map((item) => (
+          <div className=" border-b-2 border-gray-200 py-2 w-full flex flex-row justify-between text-gray-700">
+            <span>{shorten(item.title)}</span>
+            <span>$ {item.price}</span>
+          </div>
+        ))}
+        <div className=" border-b-2 border-gray-200 py-2 w-full flex flex-row justify-between text-gray-700 font-bold">
+          <span>TOTAL COST</span>
+          <span>$ {totalAmount}</span>
+        </div>
+        <form className=" py-4 text-gray-600 text-sm">
+          <div className="flex flex-row justify-start items-center py-3">
+            <input
+              type="radio"
+              value="direct"
+              checked={formik.values.paymentMethod === "direct"}
+              onChange={formik.handleChange}
+              name="paymentMethod"
+              id="direct"
+            />
+            <label htmlFor="direct" className="px-2">
+              DIRECT BANK TRANSFER
+            </label>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              erat turpis, pellentesque non leo eget, pulvinar pretium arcu.
+              Mauris porta elit non.
+            </p>
+          </div>
+          <div className="flex flex-row justify-start items-center py-3">
+            <input
+              type="radio"
+              value="cash"
+              checked={formik.values.paymentMethod === "cash"}
+              onChange={formik.handleChange}
+              name="paymentMethod"
+              id="cash"
+            />
+            <label htmlFor="cash" className="px-2">
+              CASH ON DELIVERY
+            </label>
+          </div>
+          <div className="flex flex-row justify-start items-center py-3">
+            <input
+              type="radio"
+              value="cheque"
+              checked={formik.values.paymentMethod === "cheque"}
+              onChange={formik.handleChange}
+              name="paymentMethod"
+              id="cheque"
+            />
+            <label htmlFor="cheque" className="px-2">
+              CHEQUE PAYMENT
+            </label>
+          </div>
+          <div className="flex flex-row justify-start items-center py-3">
+            <input
+              type="radio"
+              value="paypal"
+              checked={formik.values.paymentMethod === "paypal"}
+              onChange={formik.handleChange}
+              name="paymentMethod"
+              id="paypal"
+            />
+            <label htmlFor="paypal" className="px-2">
+              PAYPAL
+            </label>
+          </div>
+          <div className="flex flex-row justify-start items-center py-3 text-xs">
+            <input
+              type="radio"
+              value={true}
+              onChange={formik.handleChange}
+              name="accept"
+              id="accept"
+            />
+            <label htmlFor="accept" className="px-2">
+              I’VE READ AND ACCEPT THE
+              <span className="text-red-500 cursor-pointer">
+                {" "}TERMS & CONDITIONS
+              </span>
+            </label>
+          </div>
+          <div className="flex flex-row justify-end mt-6">
+            <button className=" bg-slate-800 py-3 px-4 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800 ">
+              Place Order
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
---
+++ src/component/checkOut/Shipping.js
@@ -0,0 +1,243 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+const Shipping = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastname: "",
+      company: "",
+      address: "",
+      city: "",
+      country: "",
+      email: "",
+      phone: "",
+    },
+    onsubmit: () => {
+      formik.setValues({
+        firstName: "",
+        lastname: "",
+        company: "",
+        address: "",
+        city: "",
+        country: "",
+        email: "",
+        phone: "",
+      });
+      formik.setTouched({}, false);
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(15, "Must be 20 characters or less")
+        .required("Required"),
+      company: Yup.string().max(15, "Must be 25 characters or less"),
+      address: Yup.string()
+        .max(15, "Must be 50 characters or less")
+        .required("Required"),
+      city: Yup.string()
+        .max(15, "Must be 30 characters or less")
+        .required("Required"),
+      country: Yup.string()
+        .max(15, "Must be 30 characters or less")
+        .required("Required"),
+      email: Yup.string().required("Required"),
+      phone: Yup.string().required("Required"),
+    }),
+  });
+  return (
+    <div className="p-4 w-full">
+      <h1 className="text-slate-800 text-2xl font-bold my-6">
+        Shipping Information
+      </h1>
+      <form
+        className="flex flex-row justify-start items-center flex-wrap"
+        onSubmit={formik.handleSubmit}
+      >
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="firstName"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            First Name:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formik.values.firstName}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.firstName}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="lastName"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Last Name:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formik.values.lastName}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.lastName}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="company"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Company Name:
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="company"
+            name="company"
+            value={formik.values.company}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.company && formik.errors.company ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.company}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="address"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Address:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="address"
+            name="address"
+            value={formik.values.address}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.address && formik.errors.address ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.address}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="city"
+            className="text-gray-600 text-sm font-semibold py-1 block "
+          >
+            City:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="city"
+            name="city"
+            value={formik.values.city}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.city && formik.errors.city ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.city}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="country"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Country:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="text"
+            id="country"
+            name="country"
+            value={formik.values.country}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.country && formik.errors.country ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.country}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="email"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Email Address:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="email"
+            id="email"
+            name="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.email}
+            </p>
+          ) : null}
+        </div>
+        <div className="h-24 w-1/2">
+          <label
+            htmlFor="phone"
+            className="text-gray-600 text-sm font-semibold py-1 block"
+          >
+            Phone:*
+          </label>
+          <input
+            className="w-4/5 outline-none py-2 px-2 border border-slate-800"
+            type="number"
+            id="phone"
+            name="phone"
+            value={formik.values.phone}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.touched.phone && formik.errors.phone ? (
+            <p className="text-red-500 text-xs font-semibold">
+              {formik.errors.phone}
+            </p>
+          ) : null}
+        </div>
+        <button className=" bg-slate-800 py-3 px-4 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Shipping;
