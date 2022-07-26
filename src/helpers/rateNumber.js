bd5dbc43ddf390ba19cf63d0c80ccbf9b34fd46c
farhad <rb0214192@gmail.com>
2022-07-26 20:07:43 +0430

    rate number helper added

---
+++ src/helpers/rateNumber.js
@@ -0,0 +1,9 @@
+import { products } from "../data/products";
+
+export const rateNumber = (rate) => {
+  if ((rate === "All")) {
+    return products.length;
+  } else {
+    return products.filter((item) => Math.floor(item.rating.rate) === rate).length;
+  }
+};
