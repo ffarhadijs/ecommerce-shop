a3d541306d6bf8e54ceb8c2e58ba4af28645abcc
farhad <rb0214192@gmail.com>
2022-07-26 16:28:14 +0430

    button component added

--- src/component/discound/Discound.js
+++ src/component/discound/Discound.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux/es/exports";
-
+import { useSelector } from "react-redux/es/exports";
+import { Link } from "react-router-dom";
+import TextButton from "../buttons/text/TextButton";
 const Discound = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const totalQuantity = useSelector((state) => state.cart.totalQuantity);
   const totalAmount = useSelector((state) => state.cart.totalAmount);
 
-  
   return (
     <div className="bg-gray-100 py-20">
       <div className=" w-5/6 mx-auto flex flex-row justify-center items-start">
@@ -14,23 +14,17 @@ const Discound = () => {
           <span className="text-xl font-semibold text-slate-800 border-b-4 border-yellow-400 pb-2 mb-24 ">
             Discound Code
           </span>
-          <div className="flex flex-row justify-center item-center bg-gray-200 px-12 py-14">
+          <div className="flex flex-row justify-center item-center bg-gray-200 px-8 py-14">
             <input
               className=" border w-80 px-3"
               type="text"
               placeholder="Enter Your Code If you Have One"
             />
-            <button className=" bg-slate-800 py-3 px-4 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800">
-              Apply Code
-            </button>
+            <TextButton text={"Apply Code"} />
           </div>
           <div className="flex flex-row justify-between item-center mt-14 w-96">
-            <button className=" bg-slate-800 py-3 px-4 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800">
-              Update Cart
-            </button>
-            <button className=" bg-slate-800 py-3 px-4 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800">
-              Continue Shopping
-            </button>
+            <TextButton text={"Update Cart"} />
+            <TextButton text="Continue Shopping" />
           </div>
         </div>
 
@@ -53,9 +47,9 @@ const Discound = () => {
                 $ {totalAmount}
               </span>
             </div>
-            <button className=" bg-slate-800 py-3 px-4 w-full font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800">
-              Proceed To CheckOut
-            </button>
+            <Link to="checkout" className=" mx-auto">
+              <TextButton text={"Proceed To CheckOut"} />
+            </Link>
           </div>
         </div>
       </div>
