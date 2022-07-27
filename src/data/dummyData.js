61cedee8653e1c26543fabb7ae7d5da086110a26
farhad <rb0214192@gmail.com>
2022-07-27 19:22:19 +0430

    some changes

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -1,9 +1,9 @@
 export const rateData = [
-  { string: "1", number: 1, id:"1" },
-  { string: "2", number: 2, id:"2" },
-  { string: "3", number: 3, id:"3" },
-  { string: "4", number: 4, id:"4" },
-  { string: "5", number: 5, id:"5" },
+  { string: "one", number: "1", id: 1 },
+  { string: "two", number: "2", id: 2 },
+  { string: "three", number: "3", id: 3 },
+  { string: "four", number: "4", id: 4 },
+  { string: "five", number: "5", id: 5 },
 ];
 
 export const categoryData = [
@@ -15,7 +15,7 @@ export const categoryData = [
 ];
 
 export const blogsCategory = [
-  { name: "All", id: 0 },
+  { name: "All Tags", id: 0 },
   { name: "Furniture", id: 1 },
   { name: "Fashion", id: 2 },
   { name: "Wood", id: 3 },
@@ -24,14 +24,12 @@ export const blogsCategory = [
 ];
 
 export const blogsYear = [
-  { year: "All", id: 0 },
-  { year: 2022, id: 1 },
-  { year: 2021, id: 2 },
-  { year: 2020, id: 3 },
-  { year: 2019, id: 4 },
-  { year: 2018, id: 5 },
+  { year: "All", id: 10 },
+  { year: "2022", id: 1 },
+  { year: "2021", id: 2 },
+  { year: "2020", id: 3 },
+  { year: "2019", id: 4 },
 ];
-
 
 export const menuList = [
   { name: "HOME", id: "1", to: "/" },
@@ -41,27 +39,26 @@ export const menuList = [
   { name: "CONTACT", id: "5", to: "/contact" },
 ];
 
-
-export const quotes=[
+export const quotes = [
   {
     name: "John Smith",
     comment:
       " Lorem ipsum dolor sit amet consectetur adipisicing elit. Maioresnon, iure exercitationem corporis ea error laudantium minimaaccusamus eius repellendus, labore earum? Fugit, adipisci possimusreiciendis soluta excepturi ut exercitationem sapiente maximealiquid distinctio corrupti, doloremque rem vero nobis non?",
     position: "Co-Funder",
-    id:"1"
+    id: "1",
   },
   {
     name: "Jack Smith",
     comment:
       " Lorem ipsum dolor sit amet consectetur adipisicing elit. Maioresnon, iure exercitationem corporis ea error laudantium minimaaccusamus eius repellendus, labore earum? Fugit, adipisci possimusreiciendis soluta excepturi ut exercitationem sapiente maximealiquid distinctio corrupti, doloremque rem vero nobis non?",
     position: "Accountant",
-    id:"2"
+    id: "2",
   },
   {
     name: "Richard Smith",
     comment:
       " Lorem ipsum dolor sit amet consectetur adipisicing elit. Maioresnon, iure exercitationem corporis ea error laudantium minimaaccusamus eius repellendus, labore earum? Fugit, adipisci possimusreiciendis soluta excepturi ut exercitationem sapiente maximealiquid distinctio corrupti, doloremque rem vero nobis non?",
     position: "Engineer",
-    id:"3"
+    id: "3",
   },
-]
+];
