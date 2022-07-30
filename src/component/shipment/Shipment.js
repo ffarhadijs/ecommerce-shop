8ba5a1958972f7dbc89079f3b74b68e08f956fd5
farhad <rb0214192@gmail.com>
2022-07-30 11:43:08 +0430

    shipment section responsived completely

--- src/component/shipment/Shipment.js
+++ src/component/shipment/Shipment.js
@@ -7,21 +7,21 @@ import { MdOutlineLocalShipping } from "react-icons/md";
 
 const Shipment = () => {
   return (
-    <div className="flex flex-row items-center justify-center py-20 bg-white">
-      <div className="text-6xl mx-8">
-        <BsBoxSeam className="mx-auto" />
+    <div className="w-full flex flex-row flex-wrap items-center justify-center py-20 bg-white">
+      <div className=" text-center w-56 my-4">
+        <BsBoxSeam className="mx-auto mb-4 text-6xl" />
         <span className="text-xl">World Wide Shipment</span>
       </div>
-      <div className="text-6xl mx-8">
-        <BsCreditCard className="mx-auto" />
+      <div className=" text-center w-56 my-4">
+        <BsCreditCard className="mx-auto mb-4 text-6xl" />
         <span className="text-xl">100% Secure Payment</span>
       </div>
-      <div className="text-6xl mx-8">
-        <BiSupport className="mx-auto" />
+      <div className=" text-center w-56 my-4">
+        <BiSupport className="mx-auto mb-4 text-6xl" />
         <span className="text-xl">24/7 online Support</span>
       </div>
-      <div className="text-6xl mx-8">
-        <MdOutlineLocalShipping className="mx-auto" />
+      <div className=" text-center w-56 my-4">
+        <MdOutlineLocalShipping className="mx-auto mb-4 text-6xl" />
         <span className="text-xl">Free Shipment Over 50$</span>
       </div>
     </div>
