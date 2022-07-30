928d9a8541d62398dab1f1a45991c65289898c56
farhad <rb0214192@gmail.com>
2022-07-30 10:31:45 +0430

    banner section redesigned and fully responsived

--- src/component/banner/Slider.js
+++ src/component/banner/Slider.js
@@ -1,20 +1,25 @@
 import React from "react";
-import banner from "../../assets/banner.jpg";
 import { Link } from "react-router-dom";
-import TextButton from "../buttons/text/TextButton";
+
 const Slider = () => {
   return (
-    <div>
-      <img alt="banner img" src={banner} className="relative w-screen h-auto" />
-      <span className=" text-4xl text-slate-800 absolute top-60 right-36 font-semibold">
-        Best Models For Summer 2022
-      </span>
-      <span className=" text-7xl text-slate-800 absolute top-80 right-40 font-extrabold">
-        look hot with<br></br> 2018 style
-      </span>
-      <Link to="/shop" className="absolute right-80" style={{ top: "550px" }}>
-        <TextButton text={"SHOP NOW"} />
-      </Link>
+    <div
+      className="banner w-full bg-right bg-contain bg-no-repeat bg-slate-100 py-8 px-5 sm:py-16 sm:px-10 text-center md:text-start"
+    >
+
+        <h1 className="sm:text-4xl text-3xl text-slate-700 font-semibold py-8">
+          Best Models For Summer 2022
+        </h1>
+        <h1 className=" text-6xl sm:text-7xl text-slate-700 font-extrabold">
+          look hot with<br></br> 2022 style
+        </h1>
+
+        <Link to="/shop">
+          <button className=" mt-16 bg-yellow-400 text-slate-800 hover:bg-slate-800 hover:text-white px-5 py-3 text-xl font-semibold transition-colors duration-300">
+            SHOP NOW
+          </button>
+        </Link>
+
     </div>
   );
 };
