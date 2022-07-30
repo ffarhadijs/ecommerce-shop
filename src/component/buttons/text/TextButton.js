c051eb090bcb243cc163d555a450a89f387746d5
farhad <rb0214192@gmail.com>
2022-07-30 10:32:33 +0430

    classname prop added to text button

--- src/component/buttons/text/TextButton.js
+++ src/component/buttons/text/TextButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const TextButton = ({ text, clickHandler }) => {
+const TextButton = ({ text, clickHandler,className }) => {
   return (
     <button
       onClick={clickHandler}
-      className="bg-slate-800 px-4 h-12 font-medium text-gray-50 hover:bg-yellow-300 hover:text-slate-800 mx-4"
+      className={`bg-slate-800 px-4 h-12 font-medium text-white hover:bg-yellow-400 hover:text-slate-800 mx-4 transition-colors duration-300 ${className}`}
     >
       {text}
     </button>
