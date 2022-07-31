4111a493ad03b0135c2a3e4b70af4b22fee038f6
farhad <rb0214192@gmail.com>
2022-07-31 09:28:23 +0430

    responssived fully

--- src/component/quickView/QuickView.js
+++ src/component/quickView/QuickView.js
@@ -15,20 +15,20 @@ const QuickView = ({ item, setIsModalOpen }) => {
       <div
         onClick={(e) => e.stopPropagation()}
         style={{ height: "500px" }}
-        className="modalContent relative flex items-center justify-center flex-row text-black z-50 w-1/2 border rounded-md bg-gray-200 py-5 px-4"
+        className="modalContent relative flex items-center justify-center flex-col md:flex-row text-black z-50 w-3/4 md:w-2/3 border rounded-md bg-gray-200 py-5 px-4"
       >
-        <div className="w-1/2 h-full bg-white flex flex-row justify-center items-center p-2">
+        <div className="lg:w-1/2 md:w-2/5 w-full h-1/2 md:h-full bg-white flex flex-row justify-center items-center p-2">
           <img src={item.image} className=" w-auto max-h-full" alt="item img" />
         </div>
-        <div className="flex flex-col justify-between w-1/2 h-full py-5 px-6 bg-white">
+        <div className="flex flex-col justify-between md:w-3/5 lg:w-1/2 w-full h-1/2 md:h-full py-5 px-6 bg-white">
           <div>
             <span className=" font-medium text-xl block">{item.title}</span>
             <span className=" block font-medium text-gray-700 py-4">
               {item.category}
             </span>
-            <span className=" block">{item.description}</span>
+            <span className=" sm:block hidden">{item.description}</span>
           </div>
-          <div className="flex flex-row p-4 items-center justify-center mx-auto">
+          <div className="flex flex-row py-4 items-center justify-center mx-auto">
             <TextButton
               text={"Add To Cart"}
               clickHandler={() => dispatch(AddToCart(item))}
