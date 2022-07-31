f0efb6800a0eb3693392f1eb88d5b2c99079d6bf
farhad <rb0214192@gmail.com>
2022-07-31 20:14:33 +0430

    responsived

--- src/component/counUp/Countup.js
+++ src/component/counUp/Countup.js
@@ -6,15 +6,15 @@ const Countup = () => {
   const [state, setState] = useState(true);
 
   return (
-    <div className="w-5/6 mx-auto py-28">
-      <div className="pb-24 mx-auto text-center">
-        <span className="text-slate-800 font-semibold text-3xl pb-4 border-b-4 border-yellow-300">
+    <div className="w-5/6 mx-auto py-14 sm:py-28">
+      <div className=" pb-12 sm:pb-24 mx-auto text-center">
+        <span className="text-slate-800 font-semibold text-2xl sm:text-3xl pb-4 border-b-4 border-yellow-300">
           Few Facts About BoShop
         </span>
       </div>
 
-      <div className=" flex flex-row justify-between items-center">
-        <div className="text-slate-800 font-bold text-8xl flex flex-col justify-center items-center">
+      <div className=" flex flex-col sm:flex-row justify-between items-center">
+        <div className="text-slate-800 font-bold text-7xl md:text-8xl flex flex-col justify-center items-center py-6">
           <CountUp
             end={457}
             duration={5}
@@ -28,9 +28,9 @@ const Countup = () => {
               </VisibilitySensor>
             )}
           </CountUp>
-          <p className="text-xl pt-8">Sales</p>
+          <p className="text-2xl pt-6">Sales</p>
         </div>
-        <div className=" text-slate-800 font-bold text-8xl flex flex-col justify-center items-center">
+        <div className=" text-slate-800 font-bold text-7xl md:text-8xl flex flex-col justify-center items-center py-6">
           <CountUp
             end={571}
             duration={5}
@@ -44,9 +44,9 @@ const Countup = () => {
               </VisibilitySensor>
             )}
           </CountUp>
-          <p className="text-xl pt-8">Items</p>
+          <p className="text-2xl pt-6">Items</p>
         </div>
-        <div className=" text-slate-800 font-bold text-8xl flex flex-col justify-center items-center">
+        <div className=" text-slate-800 font-bold text-7xl md:text-8xl flex flex-col justify-center items-center py-6">
           <CountUp
             end={289}
             duration={5}
@@ -60,7 +60,7 @@ const Countup = () => {
               </VisibilitySensor>
             )}
           </CountUp>
-          <p className="text-xl pt-8">Clients Worldwide</p>
+          <p className="text-2xl pt-6">Clients Worldwide</p>
         </div>
       </div>
     </div>
