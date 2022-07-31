7e0205b67751daa17cb9abd390cb709cdd4778e6
farhad <rb0214192@gmail.com>
2022-07-31 20:15:28 +0430

    responsived

--- src/component/cartItems/CartItems.js
+++ src/component/cartItems/CartItems.js
@@ -11,31 +11,44 @@ const CartItems = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <table className=" table-fixed w-5/6 mx-auto my-12">
+    <div className="px-4">
+      <table className=" w-full table-auto sm:w-5/6 mx-auto my-12">
         <thead className=" bg-slate-900 text-white">
           <tr>
-            <th className="py-2 w-2/5">Items</th>
+            <th className="py-2 ">Items</th>
             <th className="py-2">Price</th>
-            <th className="py-2">Qty</th>
-            <th className="py-2">Total</th>
+            <th className="py-2 ">Qty</th>
+            <th className="py-2 ">Total</th>
           </tr>
         </thead>
         <tbody>
           {cartItems.map((item) => (
             <tr className=" border-b-2" key={item.id}>
-              <td className="flex flex-row justify-start items-center p-4 text-lg text-gray-600 font-semibold">
+              <td className="flex flex-col md:flex-row justify-start items-start md:items-center p-4 text-lg text-gray-600 font-semibold">
                 <img src={item.image} className=" w-20 h-auto mx-4" />
-                {item.title}
+                <span>{item.title}</span>
               </td>
-              <td className="text-center">
+              <td className="text-center w-24">
                 $
                 <span className=" text-slate-900 font-bold text-xl">
                   {item.price}
                 </span>
               </td>
               <td className="text-center">
-                <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-col sm:flex-row items-center sm:items-center justify-center">
+                  {
+                    <Tooltip title="Increase">
+                      <button
+                        className="bg-slate-800 h-10 font-medium text-white hover:bg-yellow-300 hover:text-slate-800 mx-4 w-10 text-center"
+                        onClick={() => dispatch(Increase(item))}
+                      >
+                        <AiOutlinePlus className="mx-auto" />
+                      </button>
+                    </Tooltip>
+                  }
+                  <span className="text-slate-800 text-lg text-semibold">
+                  {item.itemQuantity}
+                  </span>
                   {item.itemQuantity <= 1 ? (
                     <Tooltip title="Remove">
                       <button
@@ -55,17 +68,6 @@ const CartItems = () => {
                       </button>
                     </Tooltip>
                   )}
-                  {item.itemQuantity}
-                  {
-                    <Tooltip title="Increase">
-                      <button
-                        className="bg-slate-800 h-10 font-medium text-white hover:bg-yellow-300 hover:text-slate-800 mx-4 w-10 text-center"
-                        onClick={() => dispatch(Increase(item))}
-                      >
-                        <AiOutlinePlus className="mx-auto" />
-                      </button>
-                    </Tooltip>
-                  }
                 </div>
               </td>
               <td className="text-center">
