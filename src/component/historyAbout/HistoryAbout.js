af0868935afd67905f9446709e2e8a1dd35d01cc
farhad <rb0214192@gmail.com>
2022-07-31 20:13:58 +0430

    responsived

--- src/component/historyAbout/HistoryAbout.js
+++ src/component/historyAbout/HistoryAbout.js
@@ -45,9 +45,9 @@ const HistoryAbout = () => {
         </div>
       </div>
       <div className=" relative flex w-full h-auto flex-row items-center justify-start pt-16">
-        <div className="absolute top-1/3 bottom-1/3 w-1/2 left-1/4 text-center text-white">
-          <h3 className="font-semibold text-2xl py-4">Awesome Work Culture</h3>
-          <span>
+        <div className="absolute sm:top-1/3 sm:bottom-1/3 w-full sm:w-1/2 sm:left-1/4 text-center text-white">
+          <h3 className="font-semibold text-xl sm:text-2xl lg:text-3xl md:pb-4 md:pt-4">Awesome Work Culture</h3>
+          <span className="lg:text-lg">
             Phasellus lacinia fermentutm bibendum. Interdum et malante ipuctus
             non. Nulla lacinia, eros vel fermentum consectetur, ris dolor in ex.
           </span>
