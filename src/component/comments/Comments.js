db23b0bf150f09b5429b320c4568c8bceb712f22
farhad <rb0214192@gmail.com>
2022-07-31 11:30:04 +0430

    comment section responsived

--- src/component/comments/Comments.js
+++ src/component/comments/Comments.js
@@ -8,8 +8,6 @@ import moment from "moment";
 import TextButton from "../buttons/text/TextButton";
 import { comments } from "../../data/dummyData";
 
-
-
 const Comments = () => {
   const formik = useFormik({
     initialValues: {
@@ -56,21 +54,30 @@ const Comments = () => {
 
   return (
     <div className="bg-white w-full flex flex-col justify-center items-center">
-      <div className="bg-white w-5/6 flex flex-col justify-center items-start mx-auto border-b-2">
-        <span className="text-slate-800 font-bold text-xl py-10">Comments</span>
+      <div className="bg-white w-full p-2 sm:p-0 sm:w-5/6 flex flex-col justify-center items-start mx-auto border-b-2">
+        <span className="text-slate-800 font-bold text-xl py-5 sm:py-10">
+          Comments
+        </span>
         {comments.map((comment) => (
-          <div className="w-2/3 flex flex-row justify-center items-center border p-4 mx-auto my-4" key={comment.id}>
-            <div className="w-1/5 p-4 ">
-              <img src={comment.img} alt="comments profile img" className="rounded-full" />
+          <div
+            className="w-full md:w-5/6 flex flex-col sm:flex-row justify-around items-start sm:items-center border p-2 mx-auto my-4"
+            key={comment.id}
+          >
+            <div className=" w-28 p-2">
+              <img
+                src={comment.img}
+                alt="comments profile img"
+                className="rounded-full"
+              />
             </div>
-            <div className="flex flex-col justify-center items-start p-4 w-4/5">
+            <div className="flex flex-col justify-center items-start p-2 w-full sm:w-4/5">
               <div className="flex flex-row justify-start items-center py-2">
-                <span className="text-slate-800 text-lg font-semibold">
+                <span className="text-slate-800 font-semibold">
                   {comment.name}
                 </span>
                 <div className="mx-4 flex flex-row justify-center items-center">
                   <AiOutlineClockCircle />
-                  <span className="font-bold text-slate-800">
+                  <span className="font-semibold text-sm text-slate-800">
                     {comment.date}
                   </span>
                 </div>
@@ -84,39 +91,44 @@ const Comments = () => {
             </div>
           </div>
         ))}
-        {comment.map((cm) => (
-          <div className="w-2/3 flex flex-row justify-center items-center border p-4 mx-auto my-4" key={cm.id}>
-            <div className="w-1/5 p-4 text-7xl flex flex-row justify-center items-center">
+        {comment.map((comment) => (
+          <div
+            className="w-full md:w-5/6 flex flex-col sm:flex-row justify-around items-start sm:items-center border p-2 mx-auto my-4"
+            key={comment.id}
+          >
+            <div className="w-28 p-2 sm:p-4 text-7xl flex flex-row justify-center items-center">
               <FaUserCircle className="mx-0 px-0 text-gray-600" />
             </div>
-            <div className="flex flex-col justify-center items-start p-4 w-4/5">
+            <div className="flex flex-col justify-center items-start p-2 w-full sm:w-4/5">
               <div className="flex flex-row justify-start items-center py-2">
-                <span className="text-slate-800 text-lg font-semibold">
-                  {cm.name}
+                <span className="text-slate-800 font-semibold">
+                  {comment.name}
                 </span>
                 <div className="mx-4 flex flex-row justify-center items-center">
                   <AiOutlineClockCircle />
-                  <span className="font-bold text-slate-800">{cm.date}</span>
+                  <span className="font-semibold text-sm text-slate-800">
+                    {comment.date}
+                  </span>
                 </div>
               </div>
-              <span className=" text-gray-500 text-sm">{cm.email}</span>
-              <p className="text-gray-700 leading-6 py-2 w-full flex flex-row flex-wrap">
-                {cm.comment}
+              <span className=" text-gray-500 text-sm">{comment.email}</span>
+              <p className="text-gray-700 leading-6 py-2 w-full">
+                {comment.comment}
               </p>
             </div>
           </div>
         ))}
       </div>
-      <div className="bg-white w-5/6 mx-auto">
-        <h2 className="text-slate-800 font-bold text-xl pt-10">
+      <div className="bg-white w-full sm:w-5/6 mx-auto">
+        <h2 className="text-slate-800 font-bold text-xl pt-10 sm:px-0 px-8">
           Post your comment
         </h2>
         <form
-          className="flex flex-col justify-center items-start py-10"
+          className="flex flex-col justify-center items-start py-10 p-4"
           onSubmit={formik.handleSubmit}
         >
-          <div className="flex flex-row justify-start items-center w-2/3 mx-auto">
-            <div className="w-1/2 mx-4 h-12">
+          <div className="flex flex-col sm:flex-row justify-start items-start dm:items-center w-full lg:w-5/6 sm:mx-auto">
+            <div className="w-4/5 sm:w-1/2 mx-4 h-24">
               <label className="text-sm" htmlFor="name">
                 Name:*
               </label>
@@ -135,7 +147,7 @@ const Comments = () => {
                 </p>
               ) : null}
             </div>
-            <div className="w-1/2 mx-4 h-12">
+            <div className="w-4/5 sm:w-1/2 mx-4 h-24">
               <label htmlFor="email" className="text-sm">
                 E-mail:*
               </label>
@@ -155,14 +167,14 @@ const Comments = () => {
               ) : null}
             </div>
           </div>
-          <div className="w-2/3 mx-auto my-10">
+          <div className="w-4/5 sm:w-3/4 lg:w-2/3 mx-4 sm:mx-auto mb-5 ">
             <label htmlFor="comment" className="text-sm">
               Comment:*
             </label>
             <textarea
               value={formik.values.comment}
               id="comment"
-              className=" resize-none w-full h-40 max-h-60 mx-auto border p-2 outline-none"
+              className=" resize-none w-full h-40 max-h-60 sm:mx-auto border p-2 outline-none"
               name="comment"
               onBlur={formik.handleBlur}
               onChange={formik.handleChange}
