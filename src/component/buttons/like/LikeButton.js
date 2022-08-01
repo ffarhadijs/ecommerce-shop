eab567c3e938caea2516029102c13e1306c4bec9
farhad <rb0214192@gmail.com>
2022-08-01 10:40:09 +0430

    updated

--- src/component/buttons/like/LikeButton.js
+++ src/component/buttons/like/LikeButton.js
@@ -12,7 +12,7 @@ const LikeButton = ({ item }) => {
     <Tooltip title="Add To Wishlist">
     <button
       onClick={() => dispatch(AddToList(item))}
-      className="bg-yellow-300 text-2xl px-4 h-12 font-medium text-slate-800 hover:bg-slate-800 hover:text-gray-50 mx-4"
+      className="bg-yellow-300 text-2xl px-4 h-12 font-medium text-slate-800 active:bg-slate-800 active:text-yellow-400 sm:hover:bg-slate-800 sm:hover:text-yellow-400 mx-4 sm:transition-colors sm:duration-300"
     >
       {wishListItems.find((product) => product.id === item.id) ? (
         <AiFillHeart className="text-red-500" />
--- src/component/buttons/text/TextButton.js
+++ src/component/buttons/text/TextButton.js
@@ -4,7 +4,7 @@ const TextButton = ({ text, clickHandler,className }) => {
   return (
     <button
       onClick={clickHandler}
-      className={`bg-slate-800 px-4 h-12 font-medium text-white active:bg-yellow-400 sm:hover:bg-yellow-400 hover:text-slate-800 mx-4 transition-colors duration-300 ${className}`}
+      className={`bg-slate-800 px-4 h-12 font-medium text-white active:bg-yellow-400 active:text-slate-800 sm:hover:bg-yellow-400 sm:hover:text-slate-800 mx-4 sm:transition-colors sm:duration-300 ${className}`}
     >
       {text}
     </button>
