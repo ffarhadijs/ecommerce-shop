9bc07bf3cb6ccad1b15cc145c62b00a8887dce86
farhad <rb0214192@gmail.com>
2022-08-01 10:51:36 +0430

    quickView responsived fully

--- src/component/quickView/QuickView.js
+++ src/component/quickView/QuickView.js
@@ -17,16 +17,16 @@ const QuickView = ({ item, setIsModalOpen }) => {
         style={{ height: "500px" }}
         className="modalContent relative flex items-center justify-center flex-col md:flex-row text-black z-50 w-3/4 md:w-2/3 border rounded-md bg-gray-200 py-5 px-4"
       >
-        <div className="lg:w-1/2 md:w-2/5 w-full h-1/2 md:h-full bg-white flex flex-row justify-center items-center p-2">
+        <div className="lg:w-1/2 md:w-2/5 w-full h-1/2 sm:h-2/5 md:h-full bg-white flex flex-row justify-center items-center p-2">
           <img src={item.image} className=" w-auto max-h-full" alt="item img" />
         </div>
-        <div className="flex flex-col justify-between md:w-3/5 lg:w-1/2 w-full h-1/2 md:h-full py-5 px-6 bg-white">
+        <div className="flex flex-col justify-between md:w-3/5 lg:w-1/2 w-full h-1/2 sm:h-3/5 md:h-full py-5 px-6 bg-white">
           <div>
-            <span className=" font-medium text-xl block">{item.title}</span>
-            <span className=" block font-medium text-gray-700 py-4">
+            <span className=" font-medium text-xl md:text-xl sm:text-base block">{item.title}</span>
+            <span className=" block font-medium text-gray-700 py-4 sm:py-1 md:py-4">
               {item.category}
             </span>
-            <span className=" sm:block hidden">{item.description}</span>
+            <span className=" sm:block hidden sm:text-sm md:text-base">{item.description}</span>
           </div>
           <div className="flex flex-row py-4 items-center justify-center mx-auto">
             <TextButton
