cd81092c5662de4a5e8dd8ed13bff881a6a4455c
farhad <rb0214192@gmail.com>
2022-08-01 09:06:16 +0430

    this page responsived

--- src/pages/CheckOutPage.js
+++ src/pages/CheckOutPage.js
@@ -18,12 +18,12 @@ const CheckOutPage = () => {
         <span className="px-2">/</span>
         <span className=" text-gray-600"> Check Out</span>
       </div>
-      <div className="w-5/6 mx-auto flex flex-row justify-center items-start">
-        <div className="w-2/3">
+      <div className="w-full sm:w-5/6 mx-auto flex flex-col md:flex-row justify-center items-start px-4">
+        <div className="w-full md:w-2/3">
           <Billing />
           <Shipping />
         </div>
-        <div className="w-1/3">
+        <div className="w-full md:w-1/3">
           <Order />
         </div>
       </div>
